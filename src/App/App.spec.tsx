244e750ef37dab23cdf028ca1340eeb384b28993
Massimo De Lorenzo <massimo.delorenzo@findynamic.com>
2024-08-13 17:57:54 +0200

    ARNA-6 add library @testing-library/react-native for testing

--- src/App/App.spec.tsx
+++ src/App/App.spec.tsx
@@ -9,9 +9,8 @@ import App from './App';
 // Note: import explicitly to use the types shipped with jest.
 import {it} from '@jest/globals';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import {render} from '@testing-library/react-native';
 
 it('renders correctly', () => {
-    renderer.create(<App />);
+    render(<App />);
 });
